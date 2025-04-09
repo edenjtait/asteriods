81b5e9d34d5f6a694a2e628875fb125433fc182d
Eden <edenjtait@gmail.com>
2025-04-09 10:50:34 -0700

    we got asteroids now

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import pygame as pyg
+import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-screen = pyg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pyg.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
 dt = 0
 
 def main():
@@ -11,22 +13,31 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # Initialize the clock
-    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteriods = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteriods, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    asteroid_field = AsteroidField()
+
     while(True):
-        for event in pyg.event.get():
-            if event.type == pyg.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 return
 
-        # Update dt at the beginning of each iteration
         dt = clock.tick(60) / 1000
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
-        pyg.display.flip()
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -4,8 +4,7 @@ from constants import *
 
 class Player(CircleShape):
   def __init__(self, x, y):
-    self.position = [x, y]
-    self.radius = PLAYER_RADIUS
+    super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
 
   def triangle(self):
